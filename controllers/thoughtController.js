dff811aeb8e89fecdf822444d515d8fef8d114e7
Maddie <maddieeckstrom@gmail.com>
2023-12-14 12:28:15 -1000

    get, post, put, and delete thoughts added

---
+++ controllers/thoughtController.js
@@ -0,0 +1,56 @@
+const { Thought } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getThoughtById(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId }).select(''); // WHAT TO PUT HERE
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought could be found with that Id'});
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async postThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async putThoughtById(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                res.status(404).json({ message: 'No thought with that id' });
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteThoughtById(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                res.status(404).json({ message: 'No thought with that id' });
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+}
