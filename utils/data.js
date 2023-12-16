fd472b5d9f6fd7832907329744d2d65f18b1c8a6
Maddie <maddieeckstrom@gmail.com>
2023-12-16 11:46:10 -1000

    added basic data to be seeded

---
+++ utils/data.js
@@ -0,0 +1,29 @@
+const users = [
+    {
+        username: 'Scrubs',
+        email: 'scrubbles@email.com',
+    },
+    {
+        username: 'X',
+        email: 'willex@email.com',
+    },
+    {
+        username: 'CC',
+        email: 'CC@email.com'
+    }
+]
+
+const thoughts = [
+    {
+        thoughtText: 'Congratulations to my brother for getting engaged',
+        username: 'Scrubs',
+    },
+    {
+        thoughtText: 'Has anyone tried the new Avatar Pandora game on Steam?',
+        username: 'X',
+    },
+    {
+        thoughtText: 'Tried the lasagna soup trend, it was delicious',
+        username: 'CC',
+    }
+]
